fd496ccf53ea3cb9677479a73524f33ddfce77a7
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-08-28 14:10:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/frontEnd_tasksPro">
+        <BrowserRouter basename="/FSOn_69_frontEnd_tasksPro">
           <ThemedApp />
         </BrowserRouter>
       </PersistGate>
